4f7a3397b7933e49e3a961a25269ad354f7a7c50
haedoang <haedoang@gmail.com>
2021-11-08 23:59:21 +0900

    docs: update ReadMe.md
    - add part6 lecture

---
+++ jpa/src/main/java/Main.java
@@ -0,0 +1,53 @@
+import entity.Member;
+import entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+/**
+ * packageName : PACKAGE_NAME
+ * fileName : Main
+ * author : haedoang
+ * date : 2021/11/08
+ * description :
+ */
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setName("haedoang");
+            member.setTeam(team);
+            em.persist(member);
+//            team.getMembers().add(member); 연관관계의 주인이 아닌 경우 조회만 가능하므로 insert되지 않는다.
+
+            em.flush(); //db에 쿼리를 반영
+            em.clear(); //영속성 컨텍스트 내 캐시를 비움
+
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+            findTeam.getName();
+
+            List<Member> members = findTeam.getMembers();
+            members.stream().forEach(System.out::println);
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
---
+++ jpa/src/main/java/entity/Member.java
@@ -0,0 +1,78 @@
+package entity;
+
+import javax.persistence.*;
+
+/**
+ * packageName : entity
+ * fileName : Member
+ * author : haedoang
+ * date : 2021/11/08
+ * description :
+ */
+@Entity
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+//
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+//    public Long getTeamId() {
+//        return teamId;
+//    }
+//
+//    public void setTeamId(Long teamId) {
+//        this.teamId = teamId;
+//    }
+
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", team=" + team +
+                '}';
+    }
+}
